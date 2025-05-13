fa2eec96997f2dfa1d77281fe4d95123031b0b13
wiis22 <wiis22@student.bth.se>
2025-05-13 13:15:40 +0200

    Fixed so the test are more unique for ass_2 and ass_3

--- backend/test/ass_2/test_getUserByEmail.py
+++ backend/test/ass_2/test_getUserByEmail.py
@@ -22,7 +22,8 @@ def test_valid_email_one_user_found(user_controller, dao_mock):
     result = user_controller.get_user_by_email('test_1@example.com')
     assert result == user
 
-def test_valid_email_multiple_users_found(user_controller, dao_mock):
+
+def test_valid_email_multiple_users_found_returns_first_user(user_controller, dao_mock):
     """Test case for a valid email with multiple users found."""
     users = [
         {'email': 'test_2@example.com', 'id': 1},
@@ -34,10 +35,25 @@ def test_valid_email_multiple_users_found(user_controller, dao_mock):
     with mock.patch('builtins.print') as mock_print:
         result = user_controller.get_user_by_email('test_2@example.com')
 
-        # Check that the print function was called with the expected message
-        mock_print.assert_called_once_with('Error: more than one user found with mail test_2@example.com')
-
         assert result == users[0]
+
+
+
+def test_valid_email_multiple_users_found_log_error(user_controller, dao_mock):
+    """Test case for a valid email with multiple users found."""
+    users = [
+        {'email': 'test_2@example.com', 'id': 1},
+        {'email': 'test_2@example.com', 'id': 2},
+    ]
+    dao_mock.find.return_value = users
+
+    # Capture the print output
+    with mock.patch('builtins.print') as mock_print:
+        user_controller.get_user_by_email('test_2@example.com')
+
+        # Check that the print function was called with the expected message
+    mock_print.assert_called_once_with('Error: more than one user found with mail test_2@example.com')
+
 
 def test_valid_email_no_user_found(user_controller, dao_mock):
     """Test case for a valid email with no user found."""
--- backend/test/ass_3/conftest.py
+++ backend/test/ass_3/conftest.py
@@ -15,9 +15,29 @@ def test_db():
 
 @pytest.fixture
 def sut(test_db):
+    test_collection_name = "video_test"
+
+    test_db.drop_collection(test_collection_name)
+
+    validator = {
+        "$jsonSchema": {
+            "bsonType": "object",
+            "required": ["url"],
+            "properties": {
+                "url": {
+                    "bsonType": "string",
+                    "description": "must be a string and is required"
+                }
+            }
+        }
+    }
+
+    test_db.create_collection(test_collection_name, validator=validator)
+
     with patch('src.util.dao.pymongo.MongoClient') as mock_client:
         mock_client_instance = MagicMock()
         mock_client_instance.edutask = test_db
         mock_client.return_value = mock_client_instance
-        yield DAO("video")
-        test_db.drop_collection("video")
+        yield DAO(test_collection_name)
+        
+        test_db.drop_collection(test_collection_name)
