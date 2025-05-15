4c452f99d11a857d992191f9e41635d9f15e7d03
wiis22 <wiis22@student.bth.se>
2025-05-15 14:03:40 +0200

    changed back to original

--- backend/src/controllers/usercontroller.py
+++ backend/src/controllers/usercontroller.py
@@ -2,7 +2,7 @@ from src.controllers.controller import Controller
 from src.util.dao import DAO
 
 import re
-emailValidator = re.compile(r"^[a-zA-Z0-9._]+@[a-zA-Z.-]+\.[a-zA-Z]{2,}$")
+emailValidator = re.compile(r'.*@.*')
 
 class UserController(Controller):
     def __init__(self, dao: DAO):
@@ -30,13 +30,11 @@ class UserController(Controller):
 
         try:
             users = self.dao.find({'email': email})
-
-            if not users:
-                return None
-
-            if len(users) > 1:
+            if len(users) == 1:
+                return users[0]
+            else:
                 print(f'Error: more than one user found with mail {email}')
-            return users[0]
+                return users[0]
         except Exception as e:
             raise
 
